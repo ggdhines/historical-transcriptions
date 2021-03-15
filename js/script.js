309c2f6905adbd654322664431ddcca6b8b81adf
Greg <ggdhines@gmail.com>
2021-03-15 17:22:50 -0400

    can now submit identified characters

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
 var identified_characters = [];
 
-var x;
-var y;
-var w;
-var h;
+// because top is a reserved word (but not that webstorm bothered to tell me)
+var upper = -1;
+var left = -1;
+var lower = -1;
+var right = -1;
 var chr;
 
+// this are needed when returning values back to the server
+var language_model;
+var page_id;
+var index_wrt_lang_model;
+
+// this are needed to load in the image
+// todo - could simplify this a bit
 var ship;
 var year;
 var month;
 var page;
+
 
 var upper_left_corner_x;
 var upper_left_corner_y;
@@ -17,8 +26,16 @@ var width;
 var height;
 
 function setup(){
+    // making sure to reset completely
+    upper = -1;
+    left = -1;
+    lower = -1;
+    right = -1;
+    chr = "";
+
     var canvas = document.getElementById('my_canvas');
     var ctx = canvas.getContext('2d');
+    identified_characters = []
 
     var image = new Image();
     var buffer = 20
@@ -31,10 +48,14 @@ function setup(){
 
 
 function set_image(data){
+    console.log(data)
+    language_model = data["language_model"]
+    page_id = data["page_id"]
     ship = data["ship_name"]
     year = data["year"]
     month = data["month"]
     page = data["page_number"]
+    index_wrt_lang_model = data["index_wrt_lang_model"]
 
     upper_left_corner_x = data["left"]
     upper_left_corner_y = data["top"]
@@ -43,24 +64,30 @@ function set_image(data){
     setup()
 }
 
-fetch("http://127.0.0.1:5000/getTile", {
-    method: "GET", // "GET/POST"
-    headers: {
-        "Content-Type": "application/json"
-    }
-}).then(response => response.json())
-    .then(data => set_image(data)
-    );
+function getTile() {
+    fetch("http://127.0.0.1:5000/getTile", {
+        method: "GET", // "GET/POST"
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then(response => response.json())
+        .then(data => set_image(data)
+        );
+}
 
 $(document).ready(function(){
         $('#my_canvas').Jcrop({
         onSelect: function(c){
             console.log(c);
 
-            x = parseInt(c.x);
-            y = parseInt(c.y);
-            h = parseInt(c.h);
-            w = parseInt(c.w);
+            // todo - rescale!!!
+            left = parseInt(c.x);
+            upper = parseInt(c.y);
+            // todo - do we need to switch top and bottom?
+            lower = parseInt(c.y2);
+            right = parseInt(c.x2);
+
+
         },
         allowSelect: true,
         allowMove: true,
@@ -70,20 +97,38 @@ $(document).ready(function(){
 
 })
 
+
+function submitTile() {
+    fetch("http://127.0.0.1:5000/submitTile", {
+        method: "POST", // "GET/POST"
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({"language_model":language_model, "page_id":page_id,"index_wrt_lang_model":index_wrt_lang_model,"identified_characters":identified_characters})
+    }).then(response => getTile())
+
+}
+
 function addCharacter() {
 
     var elem = document.getElementById('character');
     chr = elem.value
     elem.value = ""
 
-    if ((w > 0) && (chr != "")) {
-        identified_characters.push([x,y,w,h,chr])
+    if (chr == "empty") {
+        // todo - make sure if that you say empty, you don't enter anything else
+        identified_characters.push([null,null,null,null,"empty"])
+
+    }
+    else if ((upper != lower) && (left != right) && (chr != "")) {
+        identified_characters.push([{"top":upper,"left":left,"bottom":lower,"right":right,"character":chr}])
 
         var canvas = $("#my_canvas")[0];
         var context = canvas.getContext("2d");
 
         context.fillStyle = 'rgba(0,0,255,0.5)';
-        context.fillRect(x,y,w,h);
+        // console.log(left,top,right-top,bottom-top)
+        context.fillRect(left,upper,right-left,lower-upper);
 
         var jcrop_api = $('#my_canvas').data("Jcrop");
         jcrop_api.release()
@@ -101,3 +146,5 @@ function resetTile(){
 
     // var elem = document.getElementById('character');
 }
+
+window.onload = getTile
--- src/server.py
+++ src/server.py
@@ -1,73 +1,27 @@
 from flask import Flask, redirect, url_for, request,jsonify
 from flask_cors import CORS
-# import pyodbc
-# from flask_sqlalchemy import SQLAlchemy
-import pandas as pd
 import sqlalchemy as db
-import psycopg2
-import os
-
-# fn = "/home/ggdhines/PycharmProjects/historical-transcriptions/dataframes/latent1.cvs"
-# tiles_to_process = pd.read_csv(fn,delimiter=" ",error_bad_lines=False, engine="python",quoting=3)
 
 app = Flask(__name__)
-# app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://ghines:123456@127.0.0.1:5432/historical-transcriptions"
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 CORS(app)
-# connection = pyodbc.connect('Driver={Devart ODBC Driver for PostgreSQL};Server=.;Database=historical-transcriptions;uid=ghines;pwd=123456')
-# db = SQLAlchemy(app)
 
-# class User(db.Model):
-#     tile_index = db.Column(db.Integer, primary_key=True)
-#     top = db.Column(db.Integer, primary_key=True)
-#     left = db.Column(db.Integer, primary_key=True)
-#     bottom = db.Column(db.Integer, primary_key=True)
-#     right = db.Column(db.Integer, primary_key=True)
-#     language_model  = db.Column(db.String(40))
-#
-#     def __repr__(self):
-#         return str(self.tile_index)
-
-# engine = db.create_engine('postgres://ghines:123456@127.0.0.1:5432/historical-transcriptions')
-
-# def query_db(query, args=(), one=False):
-#     cur = conn().cursor()
-#     cur.execute(query, args)
-#     r = [dict((cur.description[i][0], value) \
-#                for i, value in enumerate(row)) for row in cur.fetchall()]
-#     cur.connection.close()
-#     return (r[0] if r else None) if one else r
 engine = db.create_engine('postgres://ghines:123456@127.0.0.1:5432/historical-transcriptions')
 
-
 @app.route("/getTile", methods=['POST', 'GET'])
-def helloWorld():
-    # conn = psycopg2.connect("dbname='historical-transcriptions' user='ghines' host='localhost' password='123456'")
-    # cur = conn.cursor(
-    # cur.execute("select * from tesseractResults limit 1")
-    # print(cur.fetchone())
-    # engine = db.create_engine('postgres://ghines:123456@127.0.0.1:5432/historical-transcriptions')
+def get_tile():
     with engine.connect() as connection:
-        result = connection.execute("select * from pages inner join tesseract_results on tesseract_results.page_id = pages.page_id where darkest_pixel < 150 limit 1")
-
+        result = connection.execute("select * from pages inner join tesseract_results on tesseract_results.page_id = pages.page_id order by confidence limit 1")
 
         d = dict(list(result)[0])
         return jsonify(d)
-        return jsonify({'result': [dict(row) for row in result]})
-        # return jsonify(result)
 
-    # print(pd.read_sql("pages",engine))
-    # return query_db("select * from tesseractResults limit 1")
-    return jsonify({"ship": "Bear-AG-29",
-                    "year": "1940",
-                    "month": "01",
-                    "page": "39",
-                    "upper_left_corner_x": 5987,
-                    "upper_left_corner_y": 1245,
-                    "width": 6045 - 5987,
-                    "height": 1341 - 1245,
-                    "character_index": 50})
+@app.route("/submitTile", methods=['POST'])
+def submit_tile():
+    payload = request.get_json()
 
+    for a in payload["identified_characters"]:
+        print(a)
+    return 'JSON Object Example'
 
 if __name__ == '__main__':
     app.run(debug = True)
