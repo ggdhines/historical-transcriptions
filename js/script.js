d0d241994a45d2f38b34b85964a95fcc33acc984
Greg <ggdhines@gmail.com>
2021-03-15 10:09:36 -0400

    adding in webpage and server

--- js/script.js
+++ js/script.js
@@ -6,21 +6,68 @@ var w;
 var h;
 var chr;
 
+var ship;
+var year;
+var month;
+var page;
+
+var upper_left_corner_x;
+var upper_left_corner_y;
+var width;
+var height;
 
 function setup(){
     var canvas = document.getElementById('my_canvas');
     var ctx = canvas.getContext('2d');
-    canvas.width = 300;
-    canvas.height = 300;
+
+    // var jcrop_api = $('#my_canvas').data("Jcrop");
+    // jcrop_api.disable()
+    // canvas.parentNode.setAttribute("style","width: "+width+"px; height: "+height+"px; position: relative; background-color: black;");
+    //
+    // jcrop_api.enable()
+    // jcrop_api.release()
+    // jcrop_api.destroy();
+    // jcrop_api.height = 700
+    // jcrop_api.init()
+    // $('#cropbox').width(500);
+
+
+    // canvas.setAttribute("style","display: block; visibility: visible; border: none; margin: 0px; padding: 0px; position: absolute; top: 0px; left: 0px; width: "+width+"px; height: "+height+"px;")
+
+    // canvas.width = width
+    // canvas.height = height
+    // ctx.width = width
+    // ctx.height = height
+    // ctx.style.height = "5";
+    // ctx.style.width = "5";
+    // ctx.width = 5;
+    // ctx.height = 5;
+    // ctx.clearRect( 0, 0, 10, 10)
+    // console.log(canvas.height)
     var image = new Image();
+    var buffer = 20
     image.onload = function() {
         // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage
-        ctx.drawImage(image, 100,100,40,40,0,0,300,300);
+        ctx.drawImage(image, upper_left_corner_x-buffer,upper_left_corner_y-buffer,width+2*buffer,height+2*buffer,0,0,200,200)
     };
-    image.src = "/home/ggdhines/bear/page9-1.png";
+    image.src = "images/"+ship+"-"+year+"-"+month+"-"+page+"-aligned.png";
 }
 // let promise = fetch("");
 // console.log(promise)
+
+function set_image(data){
+    ship = data["ship"]
+    year = data["year"]
+    month = data["month"]
+    page = data["page"]
+
+    upper_left_corner_x = data["upper_left_corner_x"]
+    upper_left_corner_y = data["upper_left_corner_y"]
+    width = data["width"]
+    height = data["height"]
+
+    setup()
+}
 
 fetch("http://127.0.0.1:5000/login", {
     method: "GET", // "GET/POST"
@@ -28,13 +75,14 @@ fetch("http://127.0.0.1:5000/login", {
         "Content-Type": "application/json"
     }
 }).then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => set_image(data)
+    );
 
 // window.onload = setup;
-setup();
+// setup();
 
 $(document).ready(function(){
-    $('#my_canvas').Jcrop({
+        $('#my_canvas').Jcrop({
         onSelect: function(c){
             console.log(c);
 
@@ -45,11 +93,15 @@ $(document).ready(function(){
         },
         allowSelect: true,
         allowMove: true,
-        allowResize: true
+        allowResize: true,
+            boxWidth: 700
     })
 
 })
 
+function getWidth() {
+    return "500"
+}
 
 function addCharacter() {
 
--- src/server.py
+++ src/server.py
@@ -1,21 +1,26 @@
 from flask import Flask, redirect, url_for, request,jsonify
 from flask_cors import CORS
+import pandas as pd
+
+fn = "/home/ggdhines/PycharmProjects/historical-transcriptions/dataframes/latent1.cvs"
+tiles_to_process = pd.read_csv(fn,delimiter=" ",error_bad_lines=False, engine="python",quoting=3)
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/login",methods = ['POST', 'GET'])
+
+@app.route("/getTile", methods=['POST', 'GET'])
 def helloWorld():
-    return jsonify({"ship": "bear",
+    return jsonify({"ship": "Bear-AG-29",
                     "year": "1940",
                     "month": "01",
-                    "page": "0002",
-                    "upper_left_corner_x": 20,
-                    "upper_left_corner_y": 20,
-                    "width": 40,
-                    "height": 40,
+                    "page": "39",
+                    "upper_left_corner_x": 5987,
+                    "upper_left_corner_y": 1245,
+                    "width": 6045 - 5987,
+                    "height": 1341 - 1245,
                     "character_index": 50})
 
 
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug = True)
