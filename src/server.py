5bba11cdfeb3003e2717c45e690921863875941c
Greg <ggdhines@gmail.com>
2021-03-17 10:39:09 -0400

    cleaning up unused code

--- src/server.py
+++ src/server.py
@@ -23,9 +23,6 @@ def get_tile():
 def submit_tile():
     payload = request.get_json()
 
-    print(payload["identified_characters"])
-    print(type(payload["identified_characters"]))
-
     df = pd.DataFrame.from_records(payload["identified_characters"])
     df["file_prefix"] = payload["file_prefix"]
     df["tesseract_model"] = payload["tesseract_model"]
@@ -35,24 +32,6 @@ def submit_tile():
 
     df.to_sql("user_results",engine.connect(),if_exists="append",index=False)
 
-
-    # with engine.connect() as connection:
-    #     print(payload)
-    #     print()
-    #     # for a in payload["identified_characters"]:
-    #     #     if a["character"] is not None:
-    #     #         c = "'" + a["character"] + "'"
-    #     #     else:
-    #     #         c = None
-    #     #
-    #     #     columns = "(tesseract_language_model,tile_index,upper,left,lower,right,character_s,page_number)"
-    #     #
-    #     #     if a["upper"] is not None:
-    #     #         upper = a["upper"]
-    #     #     else:
-    #     #         upper = "Null"
-    #     #     connection.execute(f"insert into human_results values ('{payload['language_model']}',{payload['index_wrt_lang_model']},{upper},{a['left']},{a['lower']},{a['right']},{c},{payload['page_id']})")
-    #     # connection.commit()
     return 'JSON Object Example'
 
 if __name__ == '__main__':
